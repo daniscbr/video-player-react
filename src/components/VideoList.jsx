d8eff84504330bd1e3d8103d37f1a2e83b232692
Daniela <dani.scbr0@gmail.com>
2021-03-05 18:12:27 -0600

    Video player functioning

--- src/components/VideoList.jsx
+++ src/components/VideoList.jsx
@@ -1,10 +1,12 @@
-var VideoList = () => (
+import VideoListEntry from './VideoListEntry.js';
+
+var VideoList = ({videos, handleClick}) => (
   <div className="video-list">
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
+    {videos.map(video => <VideoListEntry
+      video = {video}
+      handleClick = {handleClick}
+      key={video.id.videoId}
+    />)}
   </div>
 );
 
@@ -17,3 +19,4 @@ VideoList.propTypes = {
 // In the ES6 spec, files are "modules" and do not share a top-level scope.
 // `var` declarations will only exist globally where explicitly defined.
 export default VideoList;
+
